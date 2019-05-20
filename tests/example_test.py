e2a326e886ea9f7af3e557103a4ae36a5c7f4a46
Emma Good <egood@kth.se>
2019-05-20 17:18:24 +0200

    Add new test

--- tests/example_test.py
+++ tests/example_test.py
@@ -7,3 +7,17 @@ def test_method():
     assert x + y == 2*x, "test failed"
 
 
+def test_branch():
+    values = [0, 2, 44, 1100]
+    for x in values:
+        if x != 0:
+            if x > 5:
+                if x > 100:
+                    if x > 1100:
+                        print("not possible")
+                    print("hej")
+            else:
+                print("nej")
+        else:
+            print("reeeeeeee")
+
